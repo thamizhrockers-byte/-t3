32c8350a9aaa41c88b0f38d55bae8891112ca129
thamizhrockers-byte <thamizhrockers@gmail.com>
2026-08-30 23:06:59 +0530

    Update app_config.dart

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -3,5 +3,5 @@ class AppConfig {
   static const String googleScriptUrl = 'https://script.google.com/macros/s/AKfycbzUQRuHf9FTLIxmKrrQWQ5IxOtD_t4rO4xCqlBzEaYUDH5Wv7sWZCgdU1I4sICfP6owUA/exec';
 
   // Must match the token in your Apps Script.
-  static const String apiToken = 'CHANGE_THIS_TOKEN';
+  static const String apiToken = 'transx_2026-27-28-29-30';
 }
